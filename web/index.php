d718be666e0af39eda3306a60027cf00fec22e48
ge-o <gero.duppel@gmail.com>
2014-03-27 10:32:50 +0100

    Improvements

--- web/index.php
+++ web/index.php
@@ -17,29 +17,21 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 $app['debug'] = false;
 
 $app->boot();
-$app->post('/update', function (Request $request,$base_dir) use ($app) {
+$app->post('/update4711', function (Request $request,$base_dir) use ($app) {
     $newstatus = (float)$request->getContent();
     $app['monolog']->addInfo( $newstatus );
     $status_path = $base_dir.'state';
     $request_time = time();
-    $last_update = filemtime($status_path);
     file_put_contents($base_dir.'last_request',$request_time);
-    if($request_time-$last_update > 30)
-    {
-        if ($newstatus>=0.5)
-            file_put_contents($status_path,"besetzt");
-        elseif ($newstatus<0.5)
-            unlink($status_path);
-    }
+    if ($newstatus>=0.5)
+        file_put_contents($status_path,1);
+    elseif ($newstatus<0.5)
+        file_put_contents($status_path,0);
     return $app['twig']->render('state.twig', array());
 });
 
 $app->get('/', function ($base_dir) use ($app) {
-    $path = $base_dir.'state';
-    if(file_exists($path))
-        $state = false;
-    else
-        $state = true;
+    $state = (boolean)file_get_contents($base_dir.'state');
     $last_update = file_get_contents($base_dir.'last_request');
     return $app['twig']->render('index.twig', array('state'=>$state,'last_update'=>$last_update));
 });
