f7c1c9d44ddd85603a7ff45fa8205c999f9863ae
ge-o <gero.duppel@gmail.com>
2014-03-27 10:05:24 +0100

    Logging

--- web/index.php
+++ web/index.php
@@ -3,21 +3,28 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
 $app = new Silex\Application();
+$app['base_dir'] = __DIR__ . '/../upload/';
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/views',
 ));
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => $app['base_dir']. 'kicker.log',
+));
 $app['debug'] = false;
+
 $app->boot();
 $app->post('/update', function (Request $request) use ($app) {
-    $status_path = __DIR__.'/../upload/state';
+    $newstatus = (float)$request->getContent();
+    $app['monolog']->addInfo( $newstatus );
+    $status_path = $app['base_dir'].'state';
     $request_time = time();
     $last_update = filemtime($status_path);
-    file_put_contents(__DIR__.'/../upload/last_request',$request_time);
+    file_put_contents($app['base_dir'].'last_request',$request_time);
     if($request_time-$last_update > 30)
     {
-        $newstatus = (float)$request->getContent();
         if ($newstatus>=0.5)
             file_put_contents($status_path,"besetzt");
         elseif ($newstatus<0.5)
@@ -27,12 +34,12 @@ $app->post('/update', function (Request $request) use ($app) {
 });
 
 $app->get('/', function () use ($app) {
-    $path = __DIR__.'/../upload/state';
+    $path = $app['base_dir'].'state';
     if(file_exists($path))
         $state = false;
     else
         $state = true;
-    $last_update = file_get_contents(__DIR__.'/../upload/last_request');
+    $last_update = file_get_contents($app['base_dir'].'last_request');
     return $app['twig']->render('index.twig', array('state'=>$state,'last_update'=>$last_update));
 });
 
